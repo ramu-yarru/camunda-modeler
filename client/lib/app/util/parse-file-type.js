51013406212e508d6f3d477bf906c755c92e6c9e
Philipp Fromme <philippfromme@outlook.com>
2018-10-11 16:45:20 +0200

    fix(parse-file-type): correctly detect type
    * detection failed as soon as uri of another type was in file
    * type is now detected by checking namespace of root element
    
    Closes #944

--- client/lib/app/util/parse-file-type.js
+++ client/lib/app/util/parse-file-type.js
@@ -10,13 +10,47 @@ var TYPES = {
   cmmn: 'http://www.omg.org/spec/CMMN'
 };
 
+var defintionsNoPrefix = /[<\s]+(?:Definitions|definitions)\s+/,
+    namespaceNoPrefix = /xmlns="([^"]*)"/,
+    defintionsPrefix = /[<\s]+(.*):(?:Definitions|definitions)\s+/;
+
 
 module.exports = function parseType(file) {
 
-  var type = null;
+  var contents = file.contents,
+      type = null,
+      matches,
+      matchedNamespace,
+      namespacePrefix;
 
-  forEach(TYPES, function(ns, t) {
-    if (file.contents.indexOf(ns) !== -1) {
+  // no prefix e.g. <definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL">
+  if (defintionsNoPrefix.test(contents)) {
+    matches = contents.match(namespaceNoPrefix);
+
+    if (matches && matches[1]) {
+      matchedNamespace = matches[1];
+    }
+  }
+
+  // prefix e.g. <bpmn:definitions xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL">
+  if (defintionsPrefix.test(contents)) {
+    matches = contents.match(defintionsPrefix);
+
+    if (matches && matches[1]) {
+
+      // dynamically create regex based on matched prefix
+      namespacePrefix = new RegExp(`xmlns:${ matches[1] }="([^"]*)"`);
+
+      matches = contents.match(namespacePrefix);
+
+      if (matches && matches[1]) {
+        matchedNamespace = matches[1];
+      }
+    }
+  }
+
+  forEach(TYPES, function(uri, t) {
+    if (matchedNamespace && matchedNamespace.indexOf(uri) !== -1) {
       type = t;
     }
   });
